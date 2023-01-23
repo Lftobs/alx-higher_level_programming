f75e52c73a9348858567d2c444d444145045f5db
Lftobs <tobbytobs1@gmail.com>
2023-01-23 08:15:00 -0800

    0-103

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             print("wrong type")
             result = 0
-        except ZeroresultisionError:
+        except ZeroDivisionError:
             print("division by 0")
             result = 0
         except IndexError:
