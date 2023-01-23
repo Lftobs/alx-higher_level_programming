a30ccbecb1e86a139d7f2b1f36348472c6398ee0
Lftobs <106287048+Lftobs@users.noreply.github.com>
2023-01-23 16:00:18 +0100

    0-103

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_resultision(a, b):
+    
+    try:
+        result = a / b
+    except (TypeError, ZeroresultisionError):
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return (result)
